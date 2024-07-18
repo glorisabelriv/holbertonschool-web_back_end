76da5080c27f36f6dc6ed3fe45747eb070d3b639
glorisabelriv <glorisabel200@gmail.com>
2024-07-18 19:02:11 -0400

    file

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const result = [];
-  
-    for (const value of array) {
-      result.push(appendString + value);
-    }
-  
-    return result;
+  const result = [];
+
+  for (const value of array) {
+    result.push(appendString + value);
   }
+
+  return result;
+}
