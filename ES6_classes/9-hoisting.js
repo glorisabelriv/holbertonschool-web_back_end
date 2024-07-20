dd5c64404c0edc43fa3df49991d49973115b02d1
glorisabelriv <glorisabel200@gmail.com>
2024-07-19 21:47:56 -0400

    Fix this code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -12,8 +12,8 @@ export class HolbertonClass {
     return this._location;
   }
 }
-  
-  export class StudentHolberton {
+
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -32,14 +32,14 @@ export class HolbertonClass {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-  const student2 = new StudentHolberton('John', 'Doe', class2020);
-  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-  const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
