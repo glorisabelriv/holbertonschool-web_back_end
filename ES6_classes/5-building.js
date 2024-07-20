7a5396b0a7228ca6a6973a366580cce51f708cb8
glorisabelriv <glorisabel200@gmail.com>
2024-07-19 21:51:54 -0400

    Fix this code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,18 +2,20 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor.name !== 'Building') {
-      this.evacuationWarningMessage();
-    }
+    this.evacuationWarningMessage();
+  }
   }
 
+    // getter of the sqft
   get sqft() {
     return this._sqft;
   }
 
+    // setter of the sqft
   set sqft(sqft) {
     this._sqft = sqft;
   }
-
+    // eslint-disable-next-line
   evacuationWarningMessage() {
     throw Error('Class extending Building must override evacuationWarningMessage');
   }
