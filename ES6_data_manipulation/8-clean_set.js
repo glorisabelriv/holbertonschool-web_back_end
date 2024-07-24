5c67d3e1f374f6f5395c286b31f0111dbd2c73f0
glorisabelriv <glorisabel200@gmail.com>
2024-07-23 21:34:30 -0400

    passes lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string'
+  if (typeof startString !== 'string'
         || typeof set !== 'object' || startString.length === 0) {
     return '';
   }
@@ -7,7 +7,7 @@ export default function cleanSet(set, startString) {
   const str = [];
   for (const element of set) {
     if (element && element.startsWith(startString)) {
-    str.push(element.slice(startString.length));
+      str.push(element.slice(startString.length));
     }
   }
   return str.join('-');
