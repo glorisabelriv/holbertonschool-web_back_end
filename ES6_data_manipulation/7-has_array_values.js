224bc937375c5120c2918742175ff67237093f79
glorisabelriv <glorisabel200@gmail.com>
2024-07-23 21:31:19 -0400

    passes lint

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-    for (const element of array) {
-      if (set.has(element) === false) {
-        return false;
-      }
+  for (const element of array) {
+    if (set.has(element) === false) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
