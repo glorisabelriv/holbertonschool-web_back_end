46b734bd0569e096a7c514eca938b18715933bfe
glorisabelriv <glorisabel200@gmail.com>
2024-07-23 21:44:25 -0400

    Export a const instance of WeakMap and name it weakMap.

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -4,16 +4,16 @@ export const weakMap = new WeakMap();
 // Exporta la función queryAPI
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-// Si el endpoint no está en el WeakMap, se inicializa con 0
-  weakMap.set(endpoint, 0);
+    // Si el endpoint no está en el WeakMap, se inicializa con 0
+    weakMap.set(endpoint, 0);
   }
 
-// Incrementa el contador para el endpoint dado
+  // Incrementa el contador para el endpoint dado
   let count = weakMap.get(endpoint);
   count += 1;
   weakMap.set(endpoint, count);
 
-// Si el contador es mayor o igual a 5, lanza un error
+  // Si el contador es mayor o igual a 5, lanza un error
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
