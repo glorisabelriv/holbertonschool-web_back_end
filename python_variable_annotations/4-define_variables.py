51e0d0096bbf11afa57a2d3f94eed957bd70a31b
glorisabelriv <glorisabel200@gmail.com>
2024-08-13 00:11:52 -0400

    Define and annotate the following variables with the specified values:

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Este módulo define y anota varias variables con valores específicos.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
