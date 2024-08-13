3ced9ed83e79afea9f2fc4af19071133b7666457
glorisabelriv <glorisabel200@gmail.com>
2024-08-13 00:37:53 -0400

    Annotate the below function’s parameters and return values with the appropriate types

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,16 +4,16 @@ Este módulo proporciona una función que devuelve una lista
 de tuplas con elementos y su longitud.
 """
 
-from typing import List, Tuple, Sequence
+from typing import Iterable, Tuple, Sequence, List
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Toma una lista de secuencias y retorna una lista de tuplas.
-    Cada tupla contiene un elemento de la lista original y
+    Toma un iterable de secuencias y retorna una lista de tuplas.
+    Cada tupla contiene un elemento del iterable original y
     la longitud de ese elemento.
 
-    :param lst: Lista de secuencias (como cadenas, listas, etc.)
+    :param lst: Un iterable de secuencias (como cadenas, listas, etc.)
     :return: Lista de tuplas donde cada tupla contiene
     una secuencia y su longitud
     """
