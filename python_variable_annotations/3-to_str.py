1aeb75d3584cff016ba3d0716549633b6b6d18e4
glorisabelriv <glorisabel200@gmail.com>
2024-08-13 00:08:21 -0400

    Write a type-annotated function to_str that takes a float n as argument and returns the string representation of the float.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Proporciona una función para convertir un número flotante en cadena.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convierte un número flotante a su representación en cadena.
+
+    :param n: El número flotante
+    :return: La representación en cadena del número flotante
+    """
+    return str(n)
