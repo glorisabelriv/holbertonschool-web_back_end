8c89b11e58d496fbbb7c810ab9845f04798f405c
glorisabelriv <glorisabel200@gmail.com>
2024-08-13 00:01:58 -0400

    Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Este módulo proporciona una función para concatenar cadenas.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatena dos cadenas y devuelve el resultado.
+
+    :param str1: La primera cadena
+    :param str2: La segunda cadena
+    :return: La concatenación de las dos cadenas
+    """
+    return str1 + str2
