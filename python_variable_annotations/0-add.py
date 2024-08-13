311f6b92975c57fa51f486c8c4da9d569dbacc9a
glorisabelriv <glorisabel200@gmail.com>
2024-08-12 23:52:48 -0400

    Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,9 +2,12 @@
 """
     Esta función suma dos números enteros y devuelve el resultado.
     """
-def sumar(a: float, b: float) -> float:
-    """
-    Suma dos números de tipo float y devuelve el resultado.
-    """
+def add(a: float, b: float) -> float:
+        """
+    Suma dos números flotantes y devuelve el resultado.
 
-    return a + b
+    :param a: El primer número flotante
+    :param b: El segundo número flotante
+    :return: La suma de los dos números flotantes
+    """
+        return a + b
