a2f39d07d074c8d527c5beceb38ad006bcb981a9
glorisabelriv <glorisabel200@gmail.com>
2024-08-22 00:59:27 -0400

    include information

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int]:
     """
     Calcula el rango de índices para una paginación dada.
 
@@ -10,6 +10,14 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     :param page_size: Tamaño de la página
     :return: Una tupla que contiene el índice de inicio y el índice de fin
     """
-    start = (page - 1) * page_size
-    end = start + page_size
+    start: int
+    end: int
+
+    if page == 1:
+        start = 0
+    else:
+        start = (page - 1) * page_size
+
+    end = page * page_size
+
     return (start, end)
