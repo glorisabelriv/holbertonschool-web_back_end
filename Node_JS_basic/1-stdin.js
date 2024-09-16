80714aae8d51849d48115ad7d93f73f0a417e3ad
glorisabelriv <glorisabel200@gmail.com>
2024-09-16 17:36:44 -0400

    Create a program named 1-stdin.js that will be executed through command line:

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
+const os = require('os'); // Importa el módulo os para manejar el salto de línea apropiado
+
 // Mostrar el mensaje inicial
-console.log("Welcome to Holberton School, what is your name?");
+console.log(`Welcome to Holberton School, what is your name?${os.EOL}`);
 
 // Leer la entrada estándar (stdin)
 process.stdin.on('data', (input) => {
     const name = input.toString().trim(); // Convertir la entrada a string y eliminar espacios adicionales
-    console.log(`Your name is: ${name}`); // Mostrar el nombre ingresado
+    console.log(`Your name is: ${name}${os.EOL}`); // Mostrar el nombre ingresado
 
     // Cerrar el programa y mostrar el mensaje final
-    console.log("This important software is now closing");
+    console.log(`This important software is now closing${os.EOL}`);
     process.exit(); // Salir del programa
 });
