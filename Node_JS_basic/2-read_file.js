ddbc52088254ad85a8bb6acf28c20dea57aa2936
glorisabelriv <glorisabel200@gmail.com>
2024-09-16 17:50:52 -0400

    Using the database database.csv (provided in project description), create a function countStudents in the file 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -38,7 +38,6 @@ function countStudents(path) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
     }
-
   } catch (error) {
     // Si ocurre un error, lanzar el mensaje de error requerido
     throw new Error('Cannot load the database');
